7bf1b96e6c1afa24d15572004927df60123885da
hafsah <hafsahniaz25@gmail.com>
2026-02-22 16:35:09 +0530

    Updated API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const extractTasks = async (notes) => {
-  const res = await axios.post("http://localhost:5000/api/meetings", {
+  const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/extract`, {
     notes,
   });
   return res.data;
