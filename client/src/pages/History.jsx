09285e2baf5c99583b12b122c5c76478d8a65af2
hafsah <hafsahniaz25@gmail.com>
2026-02-22 17:04:39 +0530

    updated API route

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -7,7 +7,7 @@ export default function History() {
 
   useEffect(() => {
     const fetchMeetings = async () => {
-      const res = await axios.get("http://localhost:5000/api/meetings");
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/meetings`);
       setMeetings(res.data);
     };
     fetchMeetings();
